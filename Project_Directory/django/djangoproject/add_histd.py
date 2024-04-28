e3c2ece3db102d8d67d2634a537cc52f2724ecb4
LukeSheldon19 <131008107+LukeSheldon19@users.noreply.github.com>
2024-04-28 16:16:03 -0700

    c2

--- Project_Directory/django/djangoproject/add_histd.py
+++ Project_Directory/django/djangoproject/add_histd.py
@@ -14,7 +14,7 @@ from myApp.models import Company, Share, Data, Portfolio, Composed_of
 
 def main():
 
-    df = pd.read_csv('hist_df_1month_2.csv')
+    df = pd.read_csv('hist_df_1month_3.csv')
 
     with transaction.atomic():
 
