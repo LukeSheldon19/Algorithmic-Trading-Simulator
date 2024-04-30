460fe71c1fc8c5c9462e384150eb47b549b40932
LukeSheldon19 <131008107+LukeSheldon19@users.noreply.github.com>
2024-04-29 17:44:21 -0700

    c4 - :)

--- Project_Directory/django/djangoproject/myApp/forms.py
+++ Project_Directory/django/djangoproject/myApp/forms.py
@@ -11,3 +11,19 @@ class add_comp(forms.ModelForm):
     class Meta:
         model = Composed_of
         fields = ['portfolio_id','ticker','num_shares']
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        ticker = instance.ticker
+        num_shares = instance.num_shares
+        existing_entry = Composed_of.objects.filter(ticker=ticker).first()
+
+        if existing_entry:
+            existing_entry.num_shares += num_shares
+            if commit:
+                existing_entry.save()
+            return existing_entry
+        else:
+            if commit:
+                instance.save()
+            return instance
